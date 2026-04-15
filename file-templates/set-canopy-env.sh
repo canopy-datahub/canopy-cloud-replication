18e7fcc231c43e25c85f9c6656d7ecc3626caaed
Attila L. Egyedi <attila.egyedi@gmail.com>
2026-04-15 13:08:40 -0700

    Fix template file

--- file-templates/set-canopy-env.sh
+++ file-templates/set-canopy-env.sh
@@ -15,7 +15,7 @@ unset CANOPY_PROJECT_NAME
 export CANOPY_ENV=dev
 export CANOPY_HOME=~/CANOPY
 export CANOPY_CLOUD_REPLICATION=${CANOPY_HOME}/canopy-cloud-replication
-export CANOPY_AWS_PARAMETER_FILE=${CANOPY_HOME}/parameters-${CANOPY_ENV}-user.json
+export CANOPY_AWS_PARAMETER_FILE=${CANOPY_HOME}/aws-parameters-${CANOPY_ENV}-user.json
 export CANOPY_PROJECT_NAME=$(cat ${CANOPY_AWS_PARAMETER_FILE} | grep -o '"ProjectName": "[^"]*"' | cut -d'"' -f4)
 export CANOPY_UNIQUE_ID=$(cat ${CANOPY_AWS_PARAMETER_FILE} | grep -o '"DataHubUniqueId": "[^"]*"' | cut -d'"' -f4)
 
